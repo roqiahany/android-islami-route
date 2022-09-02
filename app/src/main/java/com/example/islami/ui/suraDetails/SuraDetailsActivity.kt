371a6d5c76c04ba3ac71f95efac1c2ed1b2b3e17
Roqia Hany <roqiahani5@gmail.com>
2022-09-02 20:27:58 +0000

    addad suraDetails screen addad quran files

---
+++ app/src/main/java/com/example/islami/ui/suraDetails/SuraDetailsActivity.kt
@@ -0,0 +1,49 @@
+package com.example.islami.ui.suraDetails
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.islami.R
+import com.example.islami.constants
+
+class SuraDetailsActivity : AppCompatActivity() {
+    lateinit var titleTextView :TextView
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: versesAdapter
+
+
+    @SuppressLint("UseSupportActionBar")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+         setContentView(R.layout.activity_sura_details)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        titleTextView =findViewById(R.id.title_text_view)
+        initRecyclerView()
+       val suraName:String = intent.getStringExtra(constants.Extra_Sura_Name) as String
+        val surapos:Int =intent.getIntExtra(constants.Extra_Sura_pos,-1)
+        titleTextView.setText(suraName)
+        readSuraFile(surapos)
+
+
+    }
+
+    private fun initRecyclerView() {
+        recyclerView=findViewById(R.id.recycler_view)
+        adapter= versesAdapter()
+        recyclerView.adapter=adapter
+    }
+
+    fun readSuraFile(pos:Int){
+        val fileName="${pos+1}.txt"
+        val fileContent = assets.open(fileName).bufferedReader().use { it.readText()}
+        val verses :List<String> =fileContent.split("\n");
+        adapter.changeData(verses)
+
+
+    }
+
+}
